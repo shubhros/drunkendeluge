17ce08c2c97b3eb757ecfee74439ad71ac5964c6
shubhros <shubhro@ionosnetworks.com>
2016-11-09 00:25:51 +0530

    till 6th commandment

--- littleSchemer/1.scm
+++ littleSchemer/1.scm
@@ -183,3 +183,179 @@
     (cond ((le? x y) 0)
           (else
            (1+ (div (minus x y) y))))))
+
+(define len
+  (lambda (lat)
+    (cond ((null? lat) 0)
+          (else
+           (1+ (len (cdr lat)))))))
+
+(define pick
+  (lambda (n lat)
+    (cond ((zero? (1- n)) (car lat))
+          (else
+           (pick (1- n) (cdr lat))))))
+
+(define rempick
+  (lambda (n lat)
+    (cond ((zero? (1- n)) (cdr lat))
+          (else
+           (cons (car lat)
+                 (rempick (1- n) (cdr lat)))))))
+
+(define no-nums
+  (lambda (lat)
+    (cond ((null? lat) '())
+          ((number? (car lat)) 
+           (no-nums (cdr lat)))
+          (else
+           (cons (car lat)
+                   (no-nums (cdr lat)))))))
+
+(define all-nums
+  (lambda (lat)
+    (cond ((null? lat) '())
+          ((number? (car lat))
+           (cons (car lat)
+                 (all-nums (cdr lat))))
+          (else
+           (all-nums (cdr lat))))))
+
+(define occur
+  (lambda (a lat)
+    (cond ((null? lat) 0)
+          ((eq? a (car lat))
+           (1+ (occur a (cdr lat))))
+          (else
+           (occur a (cdr lat))))))
+
+(define rember*
+  (lambda (a l)
+    (cond 
+     ((null? l) '())
+     ((atom? (car l))
+      (cond ((eq? (car l) a)
+             (rember* a (cdr l)))
+            (else
+             (cons (car l) 
+                   (rember* a (cdr l))))))
+     (else
+      (cons (rember* a (car l))
+            (rember*  a (cdr l)))))))
+
+(define insertR*
+  (lambda (new old l)
+    (cond 
+     ((null? l) '())
+     ((atom? (car l))
+      (cond 
+       ((eq? (car l) old)
+        (cons (car l)
+              (cons 
+               new 
+               (insertR* new old (cdr l)))))
+       (else
+        (cons (car l)
+              (insertR* new old (cdr l))))))
+     (else
+      (cons
+       (insertR* new old (car l))
+       (insertR* new old (cdr l)))))))
+
+(define occur*
+  (lambda (a l)
+    (cond 
+     ((null? l) 0)
+     ((atom? (car l))
+      (cond 
+       ((eq? (car l) a)
+        (1+ (occur* a (cdr l))))
+       (else
+        (occur* a (cdr l)))))
+     (else
+      (+ 
+       (occur* a (car l))
+       (occur* a (cdr l)))))))
+
+(define subst*
+  (lambda (a b l)
+    (cond 
+     ((null? l) '())
+     ((atom? (car l))
+      (cond 
+       ((eq? (car l) a)
+        (cons 
+         b 
+         (subst* a b (cdr l))))
+       (else
+        (cons 
+         (car l)
+         (subst* a b (cdr l))))))
+     (else
+      (cons (subst* a b (car l))
+            (subst* a b (cdr l)))))))
+
+(define insertL*
+  (lambda (new old l)
+    (cond 
+     ((null? l) '())
+     ((atom? (car l))
+      (cond 
+       ((eq? (car l) old)
+        (cons 
+         new 
+         (cons 
+          (car l) 
+          (insertL* new old (cdr l)))))
+       (else
+        (cons
+         (car l)
+         (insertL* new old (cdr  l))))))
+     (else
+      (cons (insertL* new old (car l))
+            (insertL* new old (cdr l)))))))
+
+(define member*
+  (lambda (a l)
+    (cond
+     ((null? l) #f)
+     ((atom? (car l))
+      (cond 
+       ((eq? (car l) a)
+        #t)
+       (else
+        (member* a (cdr l)))))
+     (else
+      (or (member* a (car l))
+          (member* a (cdr l)))))))
+
+(define leftmost
+  (lambda (l)
+    (cond ((atom? (car l))
+           (car l))
+          (else
+           (leftmost (car l))))))
+
+(define eqlist?
+  (lambda (l1 l2)
+    (cond
+     ((and (null? l1)
+           (null? l2)) #t)
+     ((or (null? l1)
+          (null? l2) #f))
+     ((atom? (car l1))
+      (and (atom? (car l2))
+           (eq? (car l1) (car l2))
+           (eqlist? (cdr l1) (cdr l2))))
+     (else
+      (and 
+       (not (atom? (car l2)))
+       (eqlist? (car l1) (car l2))
+       (eqlist? (cdr l1) (cdr l2)))))))
+
+(define equal
+  (lambda (s1 s2)
+    (cond ((and (atom? s1) (atom? s2)) (eq? s1 s2))
+          ((or (atom? s1) (atom? s2)) #f)
+          (else
+           (eqlist? s1 s2)))))
