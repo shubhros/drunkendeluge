431174e7916e691e7ac283ec82403ea5d77240c0
shubhros <shubhro@ionosnetworks.com>
2016-11-27 12:57:30 +0530

    chapter -6

--- littleSchemer/1.scm
+++ littleSchemer/1.scm
@@ -8,6 +8,7 @@
           ((atom? (car x)) (lat? (cdr x)))
           (else
            #f))))
+
 (define member?
   (lambda (a lat)
     (cond ((null? lat) #f)
@@ -447,3 +448,62 @@
       (edd1
        (zadd (zub1 n) m))))))
      
+
+(define set?
+  (lambda (lat)
+    (cond
+     ((null? lat) #t)
+     ((member? (car lat) (cdr lat)) #f)
+     (else
+      (set? (cdr lat))))))
+
+(define makeset
+  (lambda (lat)
+    (cond
+     ((null? lat) '())
+     (else
+      (cons
+       (car lat)
+       (makeset (multirember (car lat) (cdr lat))))))))
+
+(define subset?
+  (lambda (set1 set2)
+    (cond
+     ((null? set1) #t)
+     ((member? (car set1) set2)
+      (subset? (cdr set1) set2))
+     (else
+      #f))))
+
+(define eqset?
+  (lambda (set1 set2)
+    (and
+     (subset? set1 set2)
+     (subset? set2 set1))))
+
+(define intersect?
+  (lambda (set1 set2)
+    (cond
+     ((null? set1) #f)
+     (else (or (member? (car set1) set2)
+               (intersect? (cdr set1) set2))))))
+
+(define intersect
+  (lambda (set1 set2)
+    (cond
+     ((null? set1) '())
+     ((member? (car set1) set2)
+      (cons (car set1) (intersect (cdr set1) set2)))
+     (else
+      (intersect (cdr set1) set2)))))
+
+(define union
+  (lambda (set1 set2)
+    (cond 
+     ((null? set1) set2)
+     ((member? (car set1) set2)
+      (union (cdr set1) set2))
+     (else
+      (cons
+       (car set1)
+       (union (cdr set1) set2))))))
